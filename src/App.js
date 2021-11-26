8575bd95a6c280ca48b38873113e0cb10ebf9148
SeunggyuKim <mushon66@naver.com>
2021-11-26 19:50:56 +0900

    deploying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Main from "./components/Main";
 import Playing from "./components/routes/Playing";
 import GlobalStyle from "./styles/GlobalStyle";
-import customAxios from "./CustomAxios";
 
 const App = () => {
-  const [ip, setIp] = useState("");
-
-  const callback = (data) => {
-    setIp(data);
-  };
-
-  useEffect(() => {
-    customAxios("/ip", callback);
-  }, []);
-
   return (
     <Router>
       <GlobalStyle />
-      {/* {console.log(ip, "!!!")} */}
       <Route exact path="/" component={Main} />
       <Route path="/playing/:videoNo/:videoTitle" component={Playing} />
     </Router>
